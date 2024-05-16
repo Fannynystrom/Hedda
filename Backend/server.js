e3ae4b67e6badd7eb2b940dc5163b353281663a2
Fannynystrom <120643333+Fannynystrom@users.noreply.github.com>
2024-05-16 20:41:12 +0200

    backend configured and mongodb

--- Backend/server.js
+++ Backend/server.js
@@ -10,11 +10,6 @@ dotenv.config();
 const app = express();
 const port = 3000;
 
-console.log('MONGODB_URI:', process.env.MONGODB_URI);
-console.log('AWS_ACCESS_KEY_ID:', process.env.AWS_ACCESS_KEY_ID);
-console.log('AWS_SECRET_ACCESS_KEY:', process.env.AWS_SECRET_ACCESS_KEY);
-console.log('AWS_REGION:', process.env.AWS_REGION);
-console.log('S3_BUCKET_NAME:', process.env.S3_BUCKET_NAME);
 
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
